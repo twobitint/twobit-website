f960a480224136d5d42addf980ad755d3397121d
Justin Labak <jtlabak@gmail.com>
2023-03-20 22:06:57 -0700

    add love note

--- src/main.rs
+++ src/main.rs
@@ -10,7 +10,7 @@ async fn main() {
         .nest_service("/assets", ServeDir::new("assets"));
 
     // run it
-    let addr = SocketAddr::from(([127, 0, 0, 1], 8080));
+    let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     println!("listening on {}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
